119275e4626517abf81ee476e22a47ce0ad0e29a
Ikelso-tiempodevelopment <ikelso@tiempodevelopment.com>
2019-08-01 16:35:04 -0500

    add checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,11 +3,13 @@ package com.example;
 /**
  * This is a class.
  */
+ 
 public class Greeter {
 
   /**
    * This is a constructor.
    */
+   
   public Greeter() {
 
   }
@@ -16,7 +18,8 @@ public class Greeter {
 *@param someone the name of a person
 *@return greeting string
 */
-  public final String greet(final String someone) {
+
+  public String greet( String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
